9daae56118993d2f0cc93883150ed1a8955a89ff
Victor323232 <victorhugoabc59@gmail.com>
2022-04-19 20:38:58 -0300

    Start Commit

---
+++ lib/input.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class input extends StatefulWidget {
+  const input({Key? key}) : super(key: key);
+
+  @override
+  State<input> createState() => _inputState();
+}
+
+class _inputState extends State<input> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Data Input"),
+        backgroundColor: Color(630000)
+      )
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:datainput/input.dart';
+
+  void main() {
+    runApp(
+        MaterialApp(
+          home: input(),
+        )
+    );
+
+  }
