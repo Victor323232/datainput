6d24ec050713b65c19781c924e2f55eb70e81260
Victor323232 <victorhugoabc59@gmail.com>
2022-04-20 09:35:44 -0300

    written value confirmation controller written value confirmation controller

--- lib/input.dart
+++ lib/input.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class input extends StatefulWidget {
   const input({Key? key}) : super(key: key);
@@ -8,12 +9,13 @@ class input extends StatefulWidget {
 }
 
 class _inputState extends State<input> {
+  TextEditingController _textEditingController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Data Input"),
-        backgroundColor: Color(630000)
+        backgroundColor: Color(0xfff570600) //color oxfff prefix
       ),
        body: Column(
         children: <Widget>[
@@ -21,8 +23,27 @@ class _inputState extends State<input> {
            padding: EdgeInsets.all(32),
            child: TextField(
             keyboardType:  TextInputType.text,
+             decoration: InputDecoration(
+               labelText: "seu nome"
+             ),
+             //enabled: false,
+             //maxLength: 10,
+                //obscureText: true,
+             /*onChanged: (String texto){  //Receive everything written
+              print("valor escrito" + texto);
+             }*/
+              onSubmitted:(String texto){ //Receive everything written after confirmation
+                print("valor escrito:" + texto);
+              },
+              controller: _textEditingController ,//written value confirmation controller written value confirmation controller
          ),
-        )
+        ),
+         ElevatedButton(
+            child: Text("Salvar"),
+           onPressed: (){
+              print("valor escrito" + _textEditingController.text);
+           },
+          )
       ],
      ),
     );
